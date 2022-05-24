00032c1865b71d154f384ae34ed801e6c7f315c6
Maximiliano <maximiliano.n.mamone@gmail.com>
2022-05-24 16:58:17 +0200

    Nuovo DB

--- php/loadSong.php
+++ php/loadSong.php
@@ -8,22 +8,22 @@ $language = $_POST["songLanguage"];
 $relDate = $_POST["relDate"];
 $artist = $_POST["songArtist"];
 
-$sql_query = "select count(*) as cntSongs from song s where s.Title like ". $title ." and s.ReleaseDate = ". $relDate ." and ";
-$result = Connection::doQuery($sql_query);
-$row = mysqli_fetch_array($result);
-
-$count = $row['cntUser'];
-
-if ($count > 0) {
-    echo -1;
-}else {
-    $sql_query = "INSERT INTO `user` (`username`, `password`) VALUES ('" . $username . "', '" . $password . "')";
+    $sql_query = "select count(*) as cntSongs from song s where s.Title like ". $title ." and s.ReleaseDate = ". $relDate ." and ";
     $result = Connection::doQuery($sql_query);
+    $row = mysqli_fetch_array($result);
 
-    if ($result === TRUE) {
-        $_SESSION['username'] = $username;
-        echo 1;
-    }else
-        echo 0;
-}
+    $count = $row['cntUser'];
+
+    if ($count > 0) {
+        echo -1;
+    }else {
+        $sql_query = "INSERT INTO `user` (`username`, `password`) VALUES ('" . $username . "', '" . $password . "')";
+        $result = Connection::doQuery($sql_query);
+
+        if ($result === TRUE) {
+            $_SESSION['username'] = $username;
+            echo 1;
+        }else
+            echo 0;
+    }
 ?>
