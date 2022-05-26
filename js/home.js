53af0d41ce0963104a4338beecd6e2967a096355
Maximiliano Mamone <maximiliano.n.mamone@gmail.com>
2022-05-26 12:27:29 +0200

    Fixini

--- js/home.js
+++ js/home.js
@@ -24,8 +24,6 @@ $("#searchBtn").on("click", function(){
     }
 });
 
-$("#songArtist").multiselect();
-
 content.on('keyup blur','.alphaonly',function(){
     let node = $(this);
     node.val(node.val().replace(/[^a-z]/g,'') ); }
