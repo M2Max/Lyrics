ca33a1d47e6fdba9e974d50820596e3e175d4e7a
Martina Valenti <valenti.martina-2000@libero.it>
2022-05-27 17:21:39 +0200

    così va, c'è un problema nella query a riga 14

--- php/loadSong.php
+++ php/loadSong.php
@@ -5,12 +5,13 @@
 
     $title = $_POST["songTitle"];
     $language = $_POST["songLanguage"];
-    $relDate = $_POST["relDate"];
+    $relDate = $_POST["dateStandard"];
     $artist = $_POST["songArtist"];
     $text = $_POST["songText"];
 
 
-    $sql_query = "select count(*) as cntSongs from song s where s.Title like ". $title ." and s.ReleaseDate = ". $relDate ." and ";
+
+    /*$sql_query = "select count(*) as cntSongs from song s where s.Title like ". $title ." and s.ReleaseDate = ". $relDate ." and ";
     $result = Connection::doQuery($sql_query);
     $row = mysqli_fetch_array($result);
 
@@ -39,5 +40,27 @@
             }
         }else
             echo 0;
-    }
+    }*/
+
+    $text = str_replace("\n", "<br>", $text);
+    $sql_query = "INSERT INTO `song` (`Title`, `ReleaseDate`, `Text`, `Language`, `userAdd`) VALUES ('". $title ."', '". $relDate ."', '". $text ."', '". $language ."', '". $_SESSION["username"] ."');";
+    $result = Connection::doQuery($sql_query);
+
+    if ($result === TRUE) {
+        $flag = TRUE;
+        foreach ($artist as &$item){
+            $sql_query = "INSERT INTO `performance` (`SongTitle`, `SongReleaseDate`, `ArtistName`) VALUES ('". $title ."', '". $relDate ."', '". $item ."');";
+            $result = Connection::doQuery($sql_query);
+            if ($result === FALSE){
+                $flag = FALSE;
+            }
+        }
+        if ($flag === TRUE) {
+            echo 1;
+        }else {
+            echo 0;
+        }
+    }else
+        echo 0;
+
 ?>
