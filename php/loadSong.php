5eefbe67f195877d7d1be43bf7086395c832601c
Martina Valenti <valenti.martina-2000@libero.it>
2022-05-27 17:27:16 +0200

    problema risolto:)

--- php/loadSong.php
+++ php/loadSong.php
@@ -11,7 +11,7 @@
 
 
 
-    /*$sql_query = "select count(*) as cntSongs from song s where s.Title like ". $title ." and s.ReleaseDate = ". $relDate ." and ";
+    $sql_query = "select count(*) as cntSongs from song s where s.Title = '". $title ."' and s.ReleaseDate = '". $relDate ."'";
     $result = Connection::doQuery($sql_query);
     $row = mysqli_fetch_array($result);
 
@@ -40,27 +40,6 @@
             }
         }else
             echo 0;
-    }*/
-
-    $text = str_replace("\n", "<br>", $text);
-    $sql_query = "INSERT INTO `song` (`Title`, `ReleaseDate`, `Text`, `Language`, `userAdd`) VALUES ('". $title ."', '". $relDate ."', '". $text ."', '". $language ."', '". $_SESSION["username"] ."');";
-    $result = Connection::doQuery($sql_query);
-
-    if ($result === TRUE) {
-        $flag = TRUE;
-        foreach ($artist as &$item){
-            $sql_query = "INSERT INTO `performance` (`SongTitle`, `SongReleaseDate`, `ArtistName`) VALUES ('". $title ."', '". $relDate ."', '". $item ."');";
-            $result = Connection::doQuery($sql_query);
-            if ($result === FALSE){
-                $flag = FALSE;
-            }
-        }
-        if ($flag === TRUE) {
-            echo 1;
-        }else {
-            echo 0;
-        }
-    }else
-        echo 0;
+    }
 
 ?>
